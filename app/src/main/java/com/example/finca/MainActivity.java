08e369b70318b4a9804991b1ed80b189faa5c3d6
sebba081 <142901827+sebba081@users.noreply.github.com>
2024-08-31 21:33:13 -0400

    z

---
+++ app/src/main/java/com/example/finca/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.finca;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    Button btn_agre = (Button) findViewById(R.id.btn_agre);
+    btn_agre.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, agregar.class);
+            startActivity(intent);
+        }
+    });
+
+    Button btn_inve = (Button) findViewById(R.id.btn_inve);
+    btn_inve.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, inventario.class);
+            startActivity(intent);
+        }
+    });
+}
+}
